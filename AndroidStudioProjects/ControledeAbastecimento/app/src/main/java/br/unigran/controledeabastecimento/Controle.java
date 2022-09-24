0c6a287c2487a84a9fd71e7f453babfa83f12a52
Otavio <802.263@alunos.unigran.br>
2022-09-23 20:13:47 -0400

    erro na data

--- AndroidStudioProjects/ControledeAbastecimento/app/src/main/java/br/unigran/controledeabastecimento/Controle.java
+++ AndroidStudioProjects/ControledeAbastecimento/app/src/main/java/br/unigran/controledeabastecimento/Controle.java
@@ -1,11 +1,53 @@
 package br.unigran.controledeabastecimento;
 
+import java.util.Date;
+
 public class Controle {
 
     private Integer km;
     private Integer quant;
-    private String data;
+    private Date data;
     private Integer valor;
     private Integer id;
+
+    public Integer getKm() {
+        return km;
+    }
+
+    public void setKm(Integer km) {
+        this.km = km;
+    }
+
+    public Integer getQuant() {
+        return quant;
+    }
+
+    public void setQuant(Integer quant) {
+        this.quant = quant;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
 }
--- AndroidStudioProjects/ControledeAbastecimento/app/src/test/java/Banco_dados/ControleDB.java
+++ AndroidStudioProjects/ControledeAbastecimento/app/src/test/java/Banco_dados/ControleDB.java
@@ -1,9 +1,23 @@
 package Banco_dados;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import br.unigran.controledeabastecimento.Controle;
 
 public class ControleDB {
     private DBHelper db;
     private SQLiteOpenHelper conexao;
 
+        public ControleDB(DBHelper db){
+            this.db = db;
+        }
+
+        public void inserir(Controle controle){
+            conexao = db.getWritableDatabase();
+            ContentValues valores = new ContentValues();
+            valores.put("km", controle.getKm());
+            valores.put("quant", controle.getQuant());
+            valores.put("data", controle.getData());
+        }
 }
